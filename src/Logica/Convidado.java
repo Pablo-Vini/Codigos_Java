a9b71ca9115475db34996011e52594ccbcd10b3a
Pablo Vinícius Gonçalves Ramos <124642141+Pablo-Vini@users.noreply.github.com>
2023-09-05 17:54:52 -0300

    Estudando

---
+++ src/Logica/Convidado.java
@@ -0,0 +1,25 @@
+package Logica;
+
+public class Convidado {
+
+	private String nome;
+	private int codigo;
+	
+	public Convidado(String nome, int codigo) {
+		this.nome = nome;
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	@Override
+	public String toString() {
+		return "Convidado{" + "nome=" + nome + '\'' + ", codigo=" + codigo + '}';
+	}
+}
